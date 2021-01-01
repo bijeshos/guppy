2baa20ad5b656fcb040db921861cb6b44604c5cb
Bijesh O S <bijesh.os@gmail.com>
2021-01-01 20:22:45 +0530

    removed main file

--- main.go
+++
@@ -1,8 +0,0 @@
-package guppy
-
-import "log"
-
-//Note: This file would be removed later. Only kept for testing purpose.
-func main() {
-	log.Print("welcome to Guppy. Please import respective packages")
-}
