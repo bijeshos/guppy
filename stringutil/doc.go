8ec4dd26ea3ca7a7711ecb2d822963261d5ebc6a
Bijesh O S <bijesh.os@gmail.com>
2021-01-01 20:01:59 +0530

    added initial version of stringutil

---
+++ stringutil/doc.go
@@ -0,0 +1,2 @@
+//Package stringutil provides a set of useful string related utilities
+package stringutil
---
+++ stringutil/string_util.go
@@ -0,0 +1,31 @@
+package stringutil
+
+import "regexp"
+
+const (
+	SpecialCharsRegEx = "[~=?!+,#&@'\\(\\)\\[\\]\"]"
+	NonEnglishRegEx   = "\\W"
+	EmptyString       = ""
+)
+
+//RemoveSpecialChars removes special characters from the input string
+func RemoveSpecialChars(input string) string {
+	return RemoveByPattern(input, SpecialCharsRegEx)
+}
+
+//RemoveNonEnglishChars removes non-english characters from the input string
+func RemoveNonEnglishChars(input string) string {
+	return RemoveByPattern(input, NonEnglishRegEx)
+}
+
+//ReplaceByPattern replaces parts of the string based on given regex pattern
+func ReplaceByPattern(input, pattern, replacement string) string {
+	re := regexp.MustCompile(pattern)
+	return re.ReplaceAllString(input, replacement)
+}
+
+//RemoveByPattern removes parts of the string based on given regex pattern
+func RemoveByPattern(input, pattern string) string {
+	re := regexp.MustCompile(pattern)
+	return re.ReplaceAllString(input, EmptyString)
+}
